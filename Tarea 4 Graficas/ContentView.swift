e4b3e8c9cf7db1bc3a09e0e324c708b4647217be
Julia <maydeli.castan@iest.edu.mx>
2025-09-26 18:16:16 -0600

    Initial Commit

---
+++ Tarea 4 Graficas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tarea 4 Graficas
+//
+//  Created by WIN603 on 26/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tarea 4 Graficas/Tarea_4_GraficasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea_4_GraficasApp.swift
+//  Tarea 4 Graficas
+//
+//  Created by WIN603 on 26/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Tarea_4_GraficasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
